7469a10742e5ba4481b54b6695dce23f7b61d7f7
Gabriel <gabrielfvf@outlook.com>
2019-07-11 19:02:46 -0300

    Trocado o tema para um todo branco

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Gerador de nome inicial',
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
         home: RandomWords(),
       );
   }
