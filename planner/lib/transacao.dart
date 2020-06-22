ae36c526f06d075db4a1f85f38b7a77b3be650f7
Gabriel Vargas <gabrielfvf@outlook.com>
2020-06-22 17:55:55 -0300

    mais do curso

---
+++ planner/lib/transacao.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Transacao {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transacao({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}
