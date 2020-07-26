9e795594b70475c5a5833d0d40cc394b19f7263c
Gabriel Vargas <gabrielfvf@outlook.com>
2020-07-26 18:00:12 -0300

    inicio do modulo de uso de ferramentas nativas do celular

---
+++ greatPlaces/lib/widgets/image_input.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'dart:io';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final pickedImage =
+        await picker.getImage(source: ImageSource.camera, maxHeight: 600);
+    final pickedImageFile = File(pickedImage.path);
+    setState(() => _storedImage = pickedImageFile);
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(pickedImageFile.path);
+    final savedImage = await pickedImageFile.copy('${appDir.path}/$fileName');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No image taken',
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            icon: Icon(Icons.camera),
+            label: Text('Take picture'),
+            textColor: Theme.of(context).primaryColor,
+            onPressed: _takePicture,
+          ),
+        ),
+      ],
+    );
+  }
+}
